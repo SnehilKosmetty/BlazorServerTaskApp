063520cb89dce70132f8847695a5a5c1e5ffa2e7
SnehilKosmetty <Snehilkosmetty21@gmail.com>
2025-06-25 15:00:49 -0400

    Initial Setup is Done

--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Builder;
 using BlazorServerTaskApp.Data;
+using BlazorServerTaskApp;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<TaskService>();
 
 var app = builder.Build();
 
@@ -23,7 +27,9 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
+
 app.UseRouting();
+
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
